b0a78a4987dc0640cf343956432e42305b0f71bf
Mitchkachi <mitchellenkole@gmail.com>
2022-06-21 20:43:17 +0100

    updated

--- Yellow_taxi_updated.sql
+++ Yellow_taxi_updated.sql
@@ -1,3 +1,13 @@
+-- 1)  The average number of trips on Saturdays
+
+select (count("tpep_dropoff_datetime") / count(distinct("tpep_dropoff_datetime")))
+from
+(select "tpep_dropoff_datetime", to_char("tpep_dropoff_datetime", 'day') as "day_name"
+from "yellow_tripdata_2015-01") as dd
+where "day_name" like 'satur%'
+
+
+
 -- 2)The average fare (fare_amount) per trip on Saturdays
 
 select avg("fare_amount") as "avg_fare_amount", "day_name"
@@ -8,7 +18,7 @@ where "day_name" like 'satur%'
 group by 2
 order by 1
 
-k
+
 
 -- 3) The average duration per trip on Saturdays
 select avg("duration") as "avg_duration", "day_name"
@@ -23,7 +33,11 @@ order by 1
 
 -- 4) The average number of trips on Sundays
 
-
+select (count("tpep_dropoff_datetime") / count(distinct("tpep_dropoff_datetime")))
+from
+(select "tpep_dropoff_datetime", to_char("tpep_dropoff_datetime", 'day') as "day_name"
+from "yellow_tripdata_2015-01") as dd
+where "day_name" like 'sund%'
 
 
 -- 5) The average fare (fare_amount) per trip on Sundays
@@ -47,4 +61,3 @@ from "yellow_tripdata_2015-01") as "duration"
 where "day_name" like 'sund%'
 group by 2
 order by 1
-jjjjj
